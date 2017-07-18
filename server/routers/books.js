242ee37ec99362bf3c53a06ae24207f58197a98b
YEAMA <youssefemad99@hotmail.com>
2017-07-18 13:27:11 +0200

    Make all initializations & Plane

---
+++ server/routers/books.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var books = express.Router();
+
+//     /DASHBAORD	GET & POST -> offers = accept or reject
+//     /NEWBOOK		GET & POST
+//     /borrowbook	GET & POST
+
+
+
+module.exports = { books };
---
+++ server/routers/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var user = express.Router();
+
+
+//    /signup  GET & POST
+//    /login   GET & POST
+//    /Settings	GET & POST
+
+
+module.exports = { user };
